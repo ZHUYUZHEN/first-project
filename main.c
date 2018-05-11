e32f5c8182cb4af837de27cf9b6ef3fea66c24e4
janeju <janeju0712@gmail.com>
2018-05-11 10:59:36 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+int i,j;
+	for(i=0,j=0;i<=10;++i)
+	j=j+i;
+	printf("j=%d",j);
+	return 0;
+}
